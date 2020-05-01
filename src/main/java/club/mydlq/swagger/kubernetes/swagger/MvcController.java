15b66d1c34cfb13e23fc185ff2caae095c211ba6
dingliqiang <dingliqiang@aspirecn.com>
2020-05-01 15:20:47 +0800

    修改跳转地址

--- src/main/java/club/mydlq/swagger/kubernetes/swagger/MvcController.java
+++ src/main/java/club/mydlq/swagger/kubernetes/swagger/MvcController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MvcController {
 
-    @GetMapping("/")
+    @GetMapping
     public String root(){
         return "/doc.html";
     }
